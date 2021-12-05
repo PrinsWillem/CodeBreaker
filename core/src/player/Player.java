90a0f23540a8556bc49810020e1d4f0ae313fdce
ByAnthony <anthony.byledbal@icloud.com>
2021-12-05 10:50:59 +0000

    added player moves

--- core/src/player/Player.java
+++ core/src/player/Player.java
@@ -1,8 +1,10 @@
 package player;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.*;
 import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.utils.Array;
 import helpers.GameInfo;
 
 public class Player extends Sprite {
@@ -10,10 +12,17 @@ public class Player extends Sprite {
     private World world;
     private Body body;
 
-    public Player(World world, String name, float x, float y) {
-        super(new Texture(name));
+    private TextureAtlas playerAtlas;
+    private Animation<TextureRegion> animation;
+    private float elapsedTime;
+
+    private boolean isWalking;
+
+    public Player(World world, float x, float y) {
+        super(new Texture("Player/Player 1.png"));
         this.world = world;
         setPosition(x - getWidth() / 2f, y - getHeight() / 2f);
+        playerAtlas = new TextureAtlas("Player Animation/Player Animation.atlas");
         createBody();
     }
 
@@ -37,6 +46,34 @@ public class Player extends Sprite {
         shape.dispose();
     }
 
+    public void drawPlayerIdle(SpriteBatch batch) {
+        if (!isWalking) {
+            batch.draw(this, this.getX() - (this.getWidth() / 2),
+                    this.getY() - (this.getHeight() / 2));
+        }
+    }
+
+    public void drawPlayerAnimation(SpriteBatch batch) {
+        if (isWalking) {
+            elapsedTime += Gdx.graphics.getDeltaTime();
+            Array<TextureAtlas.AtlasRegion> frames = playerAtlas.getRegions();
+
+            for (TextureRegion frame : frames) {
+                if (body.getLinearVelocity().x < 0 && !frame.isFlipX()) {
+                    frame.flip(true, false);
+                } else if (body.getLinearVelocity().x > 0 && frame.isFlipX()) {
+                    frame.flip(true, false);
+                }
+            }
+
+            animation = new Animation<TextureRegion>(1f / 10f, playerAtlas.getRegions());
+
+            batch.draw(animation.getKeyFrame(elapsedTime,true), this.getX() - (this.getWidth() / 2),
+                    this.getY() - (this.getHeight() / 2));
+
+        }
+    }
+
     public void updatePlayer() {
         this.setPosition(body.getPosition().x * GameInfo.PPM,
                 body.getPosition().y * GameInfo.PPM);
@@ -45,4 +82,9 @@ public class Player extends Sprite {
     public Body getBody() {
         return this.body;
     }
+
+    public void setWalking(boolean walking) {
+        this.isWalking = walking;
+    }
+
 }
--- core/src/scenes/MainMenu.java
+++ core/src/scenes/MainMenu.java
@@ -5,9 +5,10 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.*;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.codeclan.game.GameMain;
 import helpers.GameInfo;
 import player.Player;
@@ -15,60 +16,59 @@ import player.Player;
 public class MainMenu implements Screen, ContactListener {
 
     private GameMain game;
+    private OrthographicCamera mainCamera;
+    private Viewport gameViewport;
+
+    private OrthographicCamera box2DCamera;
+    private Box2DDebugRenderer debugRenderer;
+
+    private World world;
+
     private Texture bg;
     private Player player;
     private Cloud cloud;
-    private World world;
-    OrthographicCamera camera;
-    private OrthographicCamera box2DCamera;
-    private Box2DDebugRenderer debugRenderer;
 
     public MainMenu(GameMain game) {
         this.game = game;
 
-//        camera = new OrthographicCamera();
-//        camera.setToOrtho(false, GameInfo.WIDTH, GameInfo.HEIGHT);
-
+        mainCamera = new OrthographicCamera(GameInfo.WIDTH, GameInfo.HEIGHT);
+        mainCamera.position.set(GameInfo.WIDTH / 2f, GameInfo.HEIGHT / 2f, 0);
+        gameViewport = new StretchViewport(GameInfo.WIDTH, GameInfo.HEIGHT, mainCamera);
         box2DCamera = new OrthographicCamera();
         box2DCamera.setToOrtho(false, GameInfo.WIDTH / GameInfo.PPM,
                 GameInfo.HEIGHT / GameInfo.PPM);
-        box2DCamera.position.set(GameInfo.WIDTH / 2, GameInfo.HEIGHT / 2, 0);
+        box2DCamera.position.set(GameInfo.WIDTH / 2f, GameInfo.HEIGHT / 2f, 0);
         debugRenderer = new Box2DDebugRenderer();
 
         world = new World(new Vector2(0, 0), true);
         world.setContactListener(this);
 
-        bg = new Texture("7 - Backgrounds/ship1.jpg");
-        player = new Player(world, "0 - Player/Player 1.png", GameInfo.WIDTH / 2,
+        bg = new Texture("Backgrounds/ship1.jpg");
+        player = new Player(world,GameInfo.WIDTH / 2,
                 GameInfo.HEIGHT / 2);
-
         cloud = new Cloud(world, "platform.png", GameInfo.WIDTH / 2,
                 GameInfo.HEIGHT / 2);
     }
 
     void update(float dt) {
 
-        float x =0, y =0;
+        float x = 0, y = 0;
 
         if(Gdx.input.isKeyPressed(Input.Keys.UP)){
             y += 1;
-//            player.getBody().applyLinearImpulse(new Vector2(-0.1f, 0),
-//                    player.getBody().getWorldCenter(), true);
         }else if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
             y -= 1;
-//            player.getBody().applyLinearImpulse(new Vector2(0.1f, 0),
-//                    player.getBody().getWorldCenter(), true);
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+        }else if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+            player.setWalking(true);
             x -= 1;
-//            player.getBody().applyLinearImpulse(new Vector2(-0.1f, 0),
-//                    player.getBody().getWorldCenter(), true);
         }else if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            player.setWalking(true);
             x += 1;
-//            player.getBody().applyLinearImpulse(new Vector2(0.1f, 0),
-//                    player.getBody().getWorldCenter(), true);
+        }else{
+            player.setWalking(false);
         }
         player.getBody().setLinearVelocity(x, y);
+
     }
 
     @Override
@@ -79,35 +79,33 @@ public class MainMenu implements Screen, ContactListener {
     @Override
     public void render(float delta) {
         update(delta);
-
-        player.updatePlayer();
-
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-//        camera.position.set(player.getX(), player.getY(), 0);
-//        camera.update();
-
-//        game.getBatch().setProjectionMatrix(camera.combined);
 
         game.getBatch().begin();
 
         game.getBatch().draw(bg, 0, 0);
-        game.getBatch().draw(player, player.getX() - (player.getWidth() / 2),
-                player.getY() - (player.getHeight() / 2));
+
+        player.drawPlayerIdle(game.getBatch());
+        player.drawPlayerAnimation(game.getBatch());
+
         game.getBatch().draw(cloud, cloud.getX() - (cloud.getWidth() / 2),
                 cloud.getY() - ((cloud.getWidth() / 2) + 8));
 
         game.getBatch().end();
 
-        debugRenderer.render(world, box2DCamera.combined);
+//        debugRenderer.render(world, box2DCamera.combined);
 
+        mainCamera.position.set(player.getX(), player.getY(), 0);
+        game.getBatch().setProjectionMatrix(mainCamera.combined);
+        mainCamera.update();
+
+        player.updatePlayer();
         world.step(Gdx.graphics.getDeltaTime(), 6, 2);
     }
 
     @Override
     public void resize(int width, int height) {
-
     }
 
     @Override
